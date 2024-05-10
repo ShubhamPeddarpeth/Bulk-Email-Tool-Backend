d8fea481c36e87f5bf06444533978e4968b41a02
ShubhamPeddarpeth <shubhampeddarpeth@gmail.com>
2024-05-10 23:52:37 +0530

    db in main file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
-import Connection from "./database/db.js";
 import routes from "./routes/route.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
@@ -11,6 +12,25 @@ app.use(express.json());
 app.use("/", routes);
 
 const PORT = 8000;
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const Connection = async () => {
+  const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-u310ugl-shard-00-00.ha9vbk4.mongodb.net:27017,ac-u310ugl-shard-00-01.ha9vbk4.mongodb.net:27017,ac-u310ugl-shard-00-02.ha9vbk4.mongodb.net:27017/?ssl=true&replicaSet=atlas-4k9ytq-shard-0&authSource=admin&retryWrites=true&w=majority&appName=bulkemail`;
+  try {
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true, // Enable index creation
+      poolSize: 10, // Example pool size, adjust as needed
+    });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Error while connecting with the database:", error);
+  }
+};
 
 Connection();
 
