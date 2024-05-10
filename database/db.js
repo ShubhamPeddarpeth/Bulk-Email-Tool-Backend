6de208bd6238d5fe1ff2cdd8569b42e2541f0a62
ShubhamPeddarpeth <shubhampeddarpeth@gmail.com>
2024-05-10 23:38:41 +0530

    db corrected

--- database/db.js
+++ database/db.js
@@ -6,25 +6,18 @@ dotenv.config();
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
-const Connection = async() => {
+const Connection = async () => {
   const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-u310ugl-shard-00-00.ha9vbk4.mongodb.net:27017,ac-u310ugl-shard-00-01.ha9vbk4.mongodb.net:27017,ac-u310ugl-shard-00-02.ha9vbk4.mongodb.net:27017/?ssl=true&replicaSet=atlas-4k9ytq-shard-0&authSource=admin&retryWrites=true&w=majority&appName=bulkemail`;
   try {
-    let timeout = 25;
-		while (mongoose.connection.readyState === 0) {
-      if (timeout === 0) {
-        console.log("timeout");
-        throw new Error("timeout occured with mongoose connection");
-      }
-      await mongoose.connect(DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      timeout--;
-    }
-    mongoose.set("strictQuery", false);
-    console.log("Database connected sucessfully");
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true, // Enable index creation
+      poolSize: 10, // Example pool size, adjust as needed
+    });
+    console.log("Database connected successfully");
   } catch (error) {
-    console.log("Error while connecting with the database ", error.message);
+    console.error("Error while connecting with the database:", error);
   }
 };
 
