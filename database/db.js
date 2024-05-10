726385fb90d63ee8fde85bc0a174bb463ce63af0
ShubhamPeddarpeth <shubhampeddarpeth@gmail.com>
2024-05-10 21:08:26 +0530

    updated code

--- database/db.js
+++ database/db.js
@@ -9,7 +9,7 @@ const PASSWORD = process.env.DB_PASSWORD;
 const Connection = () => {
   const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-u310ugl-shard-00-00.ha9vbk4.mongodb.net:27017,ac-u310ugl-shard-00-01.ha9vbk4.mongodb.net:27017,ac-u310ugl-shard-00-02.ha9vbk4.mongodb.net:27017/?ssl=true&replicaSet=atlas-4k9ytq-shard-0&authSource=admin&retryWrites=true&w=majority&appName=bulkemail`;
   try {
-    mongoose.connect(DB_URI, { useNewUrlParser: true });
+    mongoose.connect(DB_URI);
     mongoose.set("strictQuery", false);
     console.log("Database connected sucessfully");
   } catch (error) {
