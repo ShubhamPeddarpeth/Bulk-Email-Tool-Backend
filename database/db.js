7a5d3b295cc515266eb62dd87ff06b1e8c9ff1b0
ShubhamPeddarpeth <shubhampeddarpeth@gmail.com>
2024-05-10 23:03:12 +0530

    mongodb buffering time

--- database/db.js
+++ database/db.js
@@ -9,10 +9,18 @@ const PASSWORD = process.env.DB_PASSWORD;
 const Connection = async() => {
   const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-u310ugl-shard-00-00.ha9vbk4.mongodb.net:27017,ac-u310ugl-shard-00-01.ha9vbk4.mongodb.net:27017,ac-u310ugl-shard-00-02.ha9vbk4.mongodb.net:27017/?ssl=true&replicaSet=atlas-4k9ytq-shard-0&authSource=admin&retryWrites=true&w=majority&appName=bulkemail`;
   try {
-    await mongoose.connect(DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    let timeout = 25;
+		while (mongoose.connection.readyState === 0) {
+      if (timeout === 0) {
+        console.log("timeout");
+        throw new Error("timeout occured with mongoose connection");
+      }
+      await mongoose.connect(DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      timeout--;
+    }
     mongoose.set("strictQuery", false);
     console.log("Database connected sucessfully");
   } catch (error) {
